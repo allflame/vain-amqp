79df59d65215c2587d28c542b1689bd092ad28a8
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-10-07 13:32:26 +0300

    Initial commit

---
+++ src/Extension/AmqpExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-amqp
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-amqp
+ */
+
+namespace Vain\Amqp\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class AmqpExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class AmqpExtension extends AbstractExtension
+{
+}
